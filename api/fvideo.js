d7ad2e0fb4371620b5be5539f67bc444cee38d34
Xerox. <oz.ozk.takeru0000@gmail.com>
2025-10-21 20:53:00 +0900

    Update fvideo.js

--- api/fvideo.js
+++ api/fvideo.js
@@ -9,8 +9,7 @@ export default async function handler(req, res) {
     }
     const trending = await youtube.getTrending("Music");
     res.status(200).json(trending);
-  } catch (err)
- {
+  } catch (err) {
     console.error('Error in /api/fvideo:', err);
     res.status(500).json({ error: err.message });
   }
